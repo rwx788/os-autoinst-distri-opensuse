77d40da011ac10cb8e38e3e826cf20580ee0ce23
rwx788 <riafarov@suse.com>
2021-04-28 14:03:33 +0200

    Add workaround to start remote vnc on zvm

--- tests/installation/first_boot.pm
+++ tests/installation/first_boot.pm
@@ -20,9 +20,18 @@
 use strict;
 use warnings;
 use base 'bootbasetest';
+use testapi;
+use utils;
 
 sub run {
+    set_var('DESKTOP', 'textmode');
     shift->wait_boot_past_bootloader;
+    select_console('root-console');
+    script_run 'yast2 firewall services add zone=EXT service=service:xdmcp';
+    assert_script_run "sed -i -e 's|^DISPLAYMANAGER_REMOTE_ACCESS=.*|DISPLAYMANAGER_REMOTE_ACCESS=\"yes\"|' /etc/sysconfig/displaymanager";
+    script_run('rcxdm restart');
+    script_run('systemctl --no-pager restart display-manager');
+    select_console('x11');
 }
 
 sub test_flags {
